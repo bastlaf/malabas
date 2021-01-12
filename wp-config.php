ad6d442777b39574a3ea4c2af665311759592fb7
Bastien <bastienlafalize@gmail.com>
2021-01-12 09:10:03 +0100

    ok

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,9 @@ define( 'DB_CHARSET', 'utf8' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+/** Pour permettre d'installer des plugins sur Linux */
+define('FS_METHOD','direct');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
